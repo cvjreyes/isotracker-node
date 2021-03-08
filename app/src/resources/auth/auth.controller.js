f43f67fe5a7dc99672a2b8d405f5f79ad52ec4be
Alex Domínguez Ortega <alex.dominguez-ortega@external.technipenergies.com>
2021-03-08 10:19:13 +0100

    Al registrar y logear se usa md5

--- app/src/resources/auth/auth.controller.js
+++ app/src/resources/auth/auth.controller.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const sql = require("../../db.js");
+const md5 = require("md5");
 
 const login = async(req, res) => {
     const { email, password } = req.body;
@@ -8,7 +9,7 @@ const login = async(req, res) => {
       if (!results[0]){
         res.status(401).send("Username or password incorrect");
       }
-      else if(password !== results[0].password){
+      else if(md5(password) !== results[0].password){
         res.status(401).send("Username or password incorrect");
       }else{
         const token = jwt.sign({email: email, role: 'admin'}, "oXJYM2pcaH5S0LlNgirrBwHOo4s5Dbeh2MMHZNYqCvLA8fGM5OGm9ddxQM8UrGoFCzW1pyN4ZZMBj5dL");
