fe2cea7cd4bc8aff53d01a1e0e80cf1bbcfed732
Alex Domínguez Ortega <alex.dominguez-ortega@external.technipenergies.com>
2021-03-30 11:29:11 +0200

    Funcionamiento comprobado entre diseño, soporte y stress

--- app/src/resources/claim/claim.controller.js
+++ app/src/resources/claim/claim.controller.js
@@ -7,6 +7,8 @@ const singleClaim = async (req, res) => {
     const role = req.body.role
 
     var username = "";
+
+
     sql.query('SELECT * FROM users WHERE email = ?', [req.body.user], (err, results) =>{
       if (!results[0]){
         res.status(401).send("Username or password incorrect");
@@ -15,52 +17,44 @@ const singleClaim = async (req, res) => {
       }
     });
     console.log(username)
-    sql.query('SELECT * FROM hisoctrls WHERE filename = ?', [fileName], (err, results) =>{
-        if(!results[0]){
-            res.status(401).send("No files found");
-        }else{
-            let last = results[0]
-            for (let i = 1; i < results.length; i++){
-                if(results[i].updated_at > last.updated_at){
-                    last = results[i]
-                }
-            }
-            sql.query("INSERT INTO hisoctrls (filename, revision, tie, spo, sit, claimed, verifydesign, `from`, `to`, comments, user, role, created_at) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)", 
-            [fileName, 0, 0, 0, 0, 1, 0, last.from, last.to , last.comments, username, role, last.created_at], (err, results) => {
-            if (err) {
-                console.log("error: ", err);
-            }else{
-                console.log("created claim in hisoctrls");
-                sql.query("UPDATE misoctrls SET claimed = 1, verifydesign = 0, user = ?, role = ? WHERE filename = ?", [username, role, fileName], (err, results) =>{
-                    if (err) {
-                        console.log("error: ", err);
-                    }else{
-                        console.log("claimed iso " + fileName);
-                        res.status(200).send("claimed")
-                    }
-                })
-                }
-            })
-        }
-    })
-    /*
-    sql.query("INSERT INTO hisoctrls (filename, revision, tie, spo, sit, `from`, `to`, comments, user) VALUES (?,?,?,?,?,?,?,?,?)", 
-    [fileName, 0, 0, 0, 0, " ","Design", "Uploaded", user], (err, res) => {
-      if (err) {
-        console.log("error: ", err);
+
+    sql.query('SELECT * FROM misoctrls WHERE filename = ?', [fileName], (err, results) =>{
+      if (!results[0]){
+        res.status(401).send("The file does not exist");
+      }else if (results[0].claimed == 1 && role != "DesignLead" && role != "StressLead" && role != "SupportsLead" || 
+                (results[0].role == "DesignLead" || results[0].role == "StressLead" || results[0].role == "SupportsLead")){   
+        res.status(401).send("This isometric has already been claimed");
       }else{
-        console.log("created hisoctrls");
-        sql.query("INSERT INTO misoctrls (filename, isoid, revision, tie, spo, sit, `from`, `to`, comments, user) VALUES (?,?,?,?,?,?,?,?,?,?)", 
-        [req.body.fileName, req.body.fileName.split('.').slice(0, -1).join('.'), 0, 0, 0, 0, " ","Design", "Uploaded", req.body.user], (err, res) => {
-          if (err) {
-            console.log("error: ", err);
+        sql.query('SELECT * FROM hisoctrls WHERE filename = ?', [fileName], (err, results) =>{
+          if(!results[0]){
+              res.status(401).send("No files found");
           }else{
-            console.log("created misoctrls");
+              let last = results[0]
+              for (let i = 1; i < results.length; i++){
+                  if(results[i].updated_at > last.updated_at){
+                      last = results[i]
+                  }
+              }
+              sql.query("INSERT INTO hisoctrls (filename, revision, tie, spo, sit, claimed, verifydesign, `from`, `to`, comments, user, role, created_at) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)", 
+              [fileName, 0, 0, 0, 0, 1, 0, last.from, last.to , last.comments, username, role, last.created_at], (err, results) => {
+              if (err) {
+                  console.log("error: ", err);
+              }else{
+                  console.log("created claim in hisoctrls");
+                  sql.query("UPDATE misoctrls SET claimed = 1, verifydesign = 0, user = ?, role = ? WHERE filename = ?", [username, role, fileName], (err, results) =>{
+                      if (err) {
+                          console.log("error: ", err);
+                      }else{
+                          console.log("claimed iso " + fileName);
+                          res.status(200).send("claimed")
+                      }
+                  })
+                  }
+              })
           }
-        });
+      })
       }
     });
-    */
 }
 
 module.exports = {
--- app/src/resources/transactions/transaction.controller.js
+++ app/src/resources/transactions/transaction.controller.js
@@ -1,43 +1,81 @@
 const fs = require("fs");
+const { env } = require("process");
 const sql = require("../../db.js");
 
 const transaction = async (req, res) => {
 
+    console.log("Empieza la transaccion")
+
     sql.query("SELECT created_at FROM hisoctrls WHERE filename = ?", req.body.fileName, (err, results) => {
         if (!results[0]){
-            res.status(401).send("File no found");
+            res.status(401).send("File not found");
         }else{
-            let created_at = results[0].created_at
-            sql.query("INSERT INTO hisoctrls (filename, revision, tie, spo, sit, `from`, `to`, comments, user) VALUES (?,?,?,?,?,?,?,?,?)", 
-            [req.body.fileName, 0, 0, 0, 0, req.body.from, req.body.to, "Moved", req.body.user], (err, res) => {
-                if (err) {
-                    console.log("error: ", err);
+            sql.query("SELECT `to` FROM misoctrls WHERE filename = ?", req.body.fileName, (err, results) => {
+                if (!results[0]){
+                    res.status(401).send("File not found");
                 }else{
-                    console.log("created transaction");
-                    let origin_path = './app/storage/isoctrl/' + req.body.from + "/" + req.body.fileName
-                    let destiny_path = './app/storage/isoctrl/' + req.body.to + "/" + req.body.fileName
-                    let origin_attach_path = './app/storage/isoctrl/' + req.body.from + "/attach/" + req.body.fileName.split('.').slice(0, -1).join('.') + '.zip'
-                    let destiny_attach_path = './app/storage/isoctrl/' + req.body.to + "/attach/" + req.body.fileName.split('.').slice(0, -1).join('.') + '.zip'
-                    
-                    if(fs.existsSync(origin_path)){
-                        fs.rename(origin_path, destiny_path, function (err) {
-                            if (err) throw err
-                            console.log('Successfully renamed - AKA moved!')
-                        })
-                          if(fs.existsSync(origin_attach_path)){
-                            fs.rename(origin_attach_path, destiny_attach_path, function (err) {
-                                if (err) throw err
-                                console.log('Successfully renamed - AKA moved!')
-                            })
-                          }
-                    }
-                    
-                }
+                    console.log(results[0])
+                    const from = results[0].to
+                    let created_at = results[0].created_at
+                    sql.query("INSERT INTO hisoctrls (filename, revision, tie, spo, sit, `from`, `to`, comments, user) VALUES (?,?,?,?,?,?,?,?,?)", 
+                    [req.body.fileName, 0, 0, 0, 0, from, req.body.to, req.body.comment, req.body.user], (err, results) => {
+                        if (err) {
+                            console.log("error: ", err);
+                        }else{
+                            console.log("created transaction");
+                            let origin_path = './app/storage/isoctrl/' + from + "/" + req.body.fileName
+                            let destiny_path = './app/storage/isoctrl/' + req.body.to + "/" + req.body.fileName
+                            let origin_attach_path = './app/storage/isoctrl/' + from + "/attach/" + req.body.fileName.split('.').slice(0, -1).join('.') + '.zip'
+                            let destiny_attach_path = './app/storage/isoctrl/' + req.body.to + "/attach/" + req.body.fileName.split('.').slice(0, -1).join('.') + '.zip'
+                            let origin_cl_path = './app/storage/isoctrl/' + from + "/attach/" + req.body.fileName.split('.').slice(0, -1).join('.') + '-CL.pdf'
+                            let destiny_cl_path = './app/storage/isoctrl/' + req.body.to + "/attach/" + req.body.fileName.split('.').slice(0, -1).join('.') + '-CL.pdf'
 
-            });
-            res.status(200).send({
-                message: "Transaction completed: moved " + req.body.fileName + " from " + req.body.from + " to " + req.body.to
-              });
+                            if(fs.existsSync(origin_path)){
+                                fs.rename(origin_path, destiny_path, function (err) {
+                                    if (err) throw err
+ 
+                                })
+                                if(fs.existsSync(origin_attach_path)){
+                                    fs.rename(origin_attach_path, destiny_attach_path, function (err) {
+                                        if (err) throw err
+
+                                    })
+                                    if(fs.existsSync(origin_cl_path)){
+                                        fs.rename(origin_cl_path, destiny_cl_path, function (err) {
+                                            if (err) throw err
+                                            console.log('Successfully renamed - AKA moved!')
+                                        })
+                                    }
+                                }
+                            }
+
+                            let ld = 0;
+                            if (process.env.REACT_APP_IFC == "1"){
+                                if ((req.body.role == "StressLead" || req.body.role == "SupportsLead") && (from == "stress" && req.body.to == "supports" || from == "supports" && req.body.to == "stress")){
+                                    ld = 1;
+                                }
+                            }
+
+                            console.log(process.env.REACT_APP_ICF)
+                            console.log(req.body.role)
+                            console.log(from)
+                            console.log(req.body.to)
+                         
+                            sql.query("UPDATE misoctrls SET claimed = 0, verifydesign = ?, user = ?, role = ?, `from`= ?, `to`= ?, comments = ? WHERE filename = ?", [ld, "None", null, from, req.body.to, req.body.comment, req.body.fileName], (err, results) =>{
+                                if (err) {
+                                    console.log("error: ", err);
+                                }else{
+                                    console.log("iso moved" );
+                                    res.status(200).send("moved")
+                                }
+                            })
+                            
+                        }
+
+                    });
+                }
+            })
+
         }
     })
 
