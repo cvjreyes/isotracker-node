0bf38e6c759cf1cefb1680afe43a70f16b94050b
Alex Domínguez Ortega <alex.dominguez-ortega@external.technipenergies.com>
2021-05-19 10:39:42 +0200

    a

--- app/src/server.js
+++ app/src/server.js
@@ -37,5 +37,5 @@ require("./resources/verify/verify.routes.js")(app);
 
 // set port, listen for requests
 app.listen(process.env.REACT_APP_DB_PORT, () => {
-  console.log("Server is running on port "+REACT_APP_DB_PORT+".");
+  console.log("Server is running on port "+process.env.REACT_APP_DB_PORT+".");
 });
